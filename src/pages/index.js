075b7814b1f2f1aa78f3a7340bdbd7ce3188a6a7
Gavin Mogan <git@gavinmogan.com>
2022-07-16 10:44:37 -0700

    eslint fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {graphql, useStaticQuery} from 'gatsby';
-import {GatsbyImage, getImage} from "gatsby-plugin-image"
+import {GatsbyImage, getImage} from 'gatsby-plugin-image';
 import Layout from '../layout';
 import {SearchBox} from '../components/SearchBox';
 import {ListGroup} from 'react-bootstrap';
@@ -8,28 +8,28 @@ import {fuzzy} from 'fast-fuzzy';
 import {by639_1} from 'iso-language-codes';
 
 const filterVendors = (filters, vendors) => {
-  return vendors.filter(({node: vendor}) => {
-    if (filters.name) {
-      if (fuzzy(filters.name, vendor.name) < 0.5) {
-        return false;
-      }
-    }
-    if (Object.keys(filters.languages).length > 0) {
-      if (!Object.keys(filters.languages).every(lang => vendor.languages.includes(lang))) {
-        return false;
-      }
-    }
-    if (Object.keys(filters.features).length > 0) {
-      if (!Object.keys(filters.features).every(feature => vendor.features[feature])) {
-        return false;
-      }
-    }
-    return true;
-  })
-}
+    return vendors.filter(({node: vendor}) => {
+        if (filters.name) {
+            if (fuzzy(filters.name, vendor.name) < 0.5) {
+                return false;
+            }
+        }
+        if (Object.keys(filters.languages).length > 0) {
+            if (!Object.keys(filters.languages).every(lang => vendor.languages.includes(lang))) {
+                return false;
+            }
+        }
+        if (Object.keys(filters.features).length > 0) {
+            if (!Object.keys(filters.features).every(feature => vendor.features[feature])) {
+                return false;
+            }
+        }
+        return true;
+    });
+};
 
 const IndexPage = () => {
-  const {allVendorsYaml: {ALL_LANGUAGES, vendors}, allFeaturesYaml} = useStaticQuery(graphql`
+    const {allVendorsYaml: {ALL_LANGUAGES, vendors}, allFeaturesYaml} = useStaticQuery(graphql`
     query {
       allFeaturesYaml {
         ALL_FEATURES: edges {
@@ -75,65 +75,69 @@ const IndexPage = () => {
       }
     }
   `);
-  const ALL_FEATURES = allFeaturesYaml.ALL_FEATURES.map(({node}) => node);
-  const [filters, setFilters] = React.useState({
-    name: '',
-    features: {},
-    languages: {},
-  });
+    const ALL_FEATURES = allFeaturesYaml.ALL_FEATURES.map(({node}) => node);
+    const [filters, setFilters] = React.useState({
+        name: '',
+        features: {},
+        languages: {},
+    });
 
-  console.log('filters', filters);
+    console.log('filters', filters);
 
-  const filteredVendors = filterVendors(filters, vendors);
+    const filteredVendors = filterVendors(filters, vendors);
 
-  return (
-    <Layout sourcePath="src/pages/index.js">
-      <div className="container">
-        <div className="row body">
-          <SearchBox ALL_LANGUAGES={ALL_LANGUAGES} ALL_FEATURES={ALL_FEATURES} setFilters={setFilters} filters={filters} />
-          <div className="row">
-            <div className="col">
-              <h4 className="display-4">Results</h4>
-              {filteredVendors.length === 0 && <strong>No results</strong>}
-              {filteredVendors.length > 0 && (<ListGroup>
-                {filteredVendors.map(({node: vendor}) => {
-                  return (
-                    <div key={vendor.id} className="border border-dark rounded mb-3">
-                      <div className="row">
-                        <div className="col-md-3 d-none d-md-block mx-auto">
-                          <GatsbyImage image={getImage(vendor.logo)} alt={`${vendor.name} Logo`} />
+    return (
+        <Layout sourcePath="src/pages/index.js">
+            <div className="container">
+                <div className="row body">
+                    <SearchBox ALL_LANGUAGES={ALL_LANGUAGES} ALL_FEATURES={ALL_FEATURES} setFilters={setFilters} filters={filters} />
+                    <div className="row">
+                        <div className="col">
+                            <h4 className="display-4">Results</h4>
+                            {filteredVendors.length === 0 && <strong>No results</strong>}
+                            {filteredVendors.length > 0 && (<ListGroup>
+                                {filteredVendors.map(({node: vendor}) => {
+                                    return (
+                                        <div key={vendor.id} className="border border-dark rounded mb-3">
+                                            <div className="row">
+                                                <div className="col-md-3 d-none d-md-block mx-auto">
+                                                    <GatsbyImage image={getImage(vendor.logo)} alt={`${vendor.name} Logo`} />
+                                                </div>
+                                                <div className="col-md-9">
+                                                    <h3 className="display-3">{vendor.name}</h3>
+                                                    <div>
+                                                        <strong>Features: </strong>
+                                                        <ul className="list-inline d-inline-block">
+                                                            {ALL_FEATURES.filter(feature => vendor.features[feature.key]).map(feature => (
+                                                                <li className="list-inline-item" key={feature.key} style={{
+                                                                    display: 'inline-block',
+                                                                    color: vendor.features[feature.key] ? 'green' : '',
+                                                                }}>
+                                                                    {vendor.features[feature.key] && <ion-icon name="checkmark-circle" />}
+                                                                    {' '}
+                                                                    {feature.label}
+                                                                </li>
+                                                            ))}
+                                                        </ul>
+                                                    </div>
+                                                    <p>
+                                                        <strong>Languages:</strong>
+                                                        {' '}
+                                                        {vendor.languages.map(lang => by639_1[lang]?.name || lang).join(', ')}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </ListGroup>)
+                            }
                         </div>
-                        <div className="col-md-9">
-                          <h3 className="display-3">{vendor.name}</h3>
-                          <div>
-                            <strong>Features: </strong>
-                            <ul className="list-inline d-inline-block">
-                              {ALL_FEATURES.filter(feature => vendor.features[feature.key]).map(feature => (
-                                <li className="list-inline-item" key={feature.key} style={{
-                                  display: 'inline-block',
-                                  color: vendor.features[feature.key] ? 'green' : '',
-                                }}>
-                                  {vendor.features[feature.key] && <ion-icon name="checkmark-circle"></ion-icon>}
-                                  {' '}
-                                  {feature.label}
-                                </li>
-                              ))}
-                            </ul>
-                          </div>
-                          <p><strong>Languages:</strong> {vendor.languages.map(lang => by639_1[lang]?.name || lang).join(', ')}</p>
-                        </div>
-                      </div>
                     </div>
-                  );
-                })}
-              </ListGroup>)
-              }
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+        </Layout>
+    );
 };
 
 export default IndexPage;
